d2a2e5fe8cffa99bb143b15485d95e62ebd7ec20
collinlidstrom <collinlidstrom@gmail.com>
2023-05-29 01:08:04 -0500

    updated screen text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Chase!
+          Coming soon!
         </p>
         <a
           className="App-link"
